f487db9b6424bfa40824b7913256424623503fa5
Derrick Cross <decross1@me.com>
2018-05-16 13:03:30 -0700

    view and error handling for detail view started

--- polls/views.py
+++ polls/views.py
@@ -4,21 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try: 
+        question = Question.objects.get(pk=question_id)
+        context = {
+            'question': question
+        }
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
